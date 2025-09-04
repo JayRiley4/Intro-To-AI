99b5d2e84123290a357e2c85218e9f52b141a633
JayRiley4 <jaeriley@claflin.edu>
2025-09-04 14:39:21 -0400

    Add files via upload

--- Simpler-linear-regfression.R
+++ Simpler-linear-regfression.R
@@ -3,5 +3,25 @@
 # Get some dummy dataset
 cars <- mtcars
 
+head(cars)
+
 #Scatter plot on training data
 scatter.smooth(x=cars$mpg, y= cars$disp, main="SpeedVSDistance")
+
+# Calculate correlation value between mpg and disp
+cor(cars$mpg, cars$disp)
+
+# Split training and testing data for regression 
+training_data <- cars[1:16,]
+test_data <- cars[17:32,]
+
+# Build a regression model
+regression_result <- lm(mpg ~ disp, data=training_data)
+
+# predict regression function on test data
+prediction_result <- predict(regression_result,test_data)
+
+# Results of prediction 
+actual_prediction_values <-
+data.frame(cbind(actuals=training_data$mpg,
+predicateds=prediction_result))
